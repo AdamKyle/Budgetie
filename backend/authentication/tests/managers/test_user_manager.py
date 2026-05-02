6f06cb336cf7c1881eb0997cc0b803d429804ab5
Adam Balan <a-kyle@hotmail.com>
2026-05-02 17:39:21 -0400

    Added a test, we now have 100% coverage

---
+++ backend/authentication/tests/managers/test_user_manager.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from authentication.models import User
+
+
+class UserManagerTest(TestCase):
+    
+    def test_create_user_requires_email(self) -> None:
+        with self.assertRaisesMessage(ValueError, "Email is required."):
+            User.objects.create_user(
+                email="",
+                password="StrongPassword123!",
+                first_name="Test",
+                last_name="User",
+            )
